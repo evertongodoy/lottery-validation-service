596f5adaaa1e204575fe83ee9a670b17b711a2b7
evertongodoy <everton.mgodoy@gmail.com>
2026-02-10 09:07:12 -0300

    refactor on VerifyUserDrawRepositoryAdapter.java

--- src/main/java/com/lottery/validation/infrastructure/adapters/output/persistence/repositories/VerifyUserDrawRepositoryAdapter.java
+++ src/main/java/com/lottery/validation/infrastructure/adapters/output/persistence/repositories/VerifyUserDrawRepositoryAdapter.java
@@ -11,19 +11,17 @@ import com.lottery.validation.infrastructure.adapters.output.persistence.mongodb
 import com.lottery.validation.infrastructure.adapters.output.persistence.mongodb.WinnersUserDrawMongoRepository;
 import com.lottery.validation.infrastructure.config.LotteryApiProperties;
 
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
 @Component
 public class VerifyUserDrawRepositoryAdapter implements VerifyUserDrawOutputPort {
-
-    private static final ParameterizedTypeReference<Map<String, Object>> MAP_TYPE = new ParameterizedTypeReference<>() {};
 
     private final UserDrawMongoRepository userDrawMongoRepository;
     private final UserDrawPersistenceMapper userDrawPersistenceMapper;
@@ -47,16 +45,15 @@ public class VerifyUserDrawRepositoryAdapter implements VerifyUserDrawOutputPort
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Map<String, Object> findWebLottery(LotteryType lotteryType) {
         log.info("[findWebLottery] | lotteryType={}", lotteryType);
         String lotteryPath = getLotteryPath(lotteryType);
         String url = String.format("%s/%s", lotteryApiProperties.getUrl(), lotteryPath);
         
         var response = restTemplate.getForObject(url, Map.class);
-        if (response == null) {
-            throw new RuntimeException("Failed to fetch latest draw from API");
-        }
-        return response;
+        return Optional.ofNullable(response)
+                .orElseThrow(() -> new RuntimeException("Failed to fetch latest draw from API"));
     }
 
     @Override
