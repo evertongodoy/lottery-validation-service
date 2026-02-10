1f433a4c9d42e25f8bac1ed72289087a94cbb1b8
evertongodoy <everton.mgodoy@gmail.com>
2026-02-10 00:18:14 -0300

    added lombok Data, Builder, NoArgsConstructor and AllArgsConstructor

--- src/main/java/com/lottery/validation/infrastructure/adapters/output/persistence/entities/LotteryEntity.java
+++ src/main/java/com/lottery/validation/infrastructure/adapters/output/persistence/entities/LotteryEntity.java
@@ -1,6 +1,12 @@
 package com.lottery.validation.infrastructure.adapters.output.persistence.entities;
 
 import com.lottery.validation.domain.enums.LotteryType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -10,6 +16,10 @@ import java.util.List;
 import java.util.UUID;
 
 @Document(collection = "lottery")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LotteryEntity {
 
     @Id
@@ -42,101 +52,4 @@ public class LotteryEntity {
     @Field("lottery_type")
     private LotteryType lotteryType;
 
-    public LotteryEntity() {
-    }
-
-    public LotteryEntity(String id, UUID internalId, Integer lotteryNumber, Integer nextLotteryNumber,
-                         LocalDate addAt, LocalDate drawDate, List<Integer> drawnNumbers,
-                         List<Integer> sortedDrawNumbers, String city, LotteryType lotteryType) {
-        this.id = id;
-        this.internalId = internalId;
-        this.lotteryNumber = lotteryNumber;
-        this.nextLotteryNumber = nextLotteryNumber;
-        this.addAt = addAt;
-        this.drawDate = drawDate;
-        this.drawnNumbers = drawnNumbers;
-        this.sortedDrawNumbers = sortedDrawNumbers;
-        this.city = city;
-        this.lotteryType = lotteryType;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public UUID getInternalId() {
-        return internalId;
-    }
-
-    public void setInternalId(UUID internalId) {
-        this.internalId = internalId;
-    }
-
-    public Integer getLotteryNumber() {
-        return lotteryNumber;
-    }
-
-    public void setLotteryNumber(Integer lotteryNumber) {
-        this.lotteryNumber = lotteryNumber;
-    }
-
-    public Integer getNextLotteryNumber() {
-        return nextLotteryNumber;
-    }
-
-    public void setNextLotteryNumber(Integer nextLotteryNumber) {
-        this.nextLotteryNumber = nextLotteryNumber;
-    }
-
-    public LocalDate getAddAt() {
-        return addAt;
-    }
-
-    public void setAddAt(LocalDate addAt) {
-        this.addAt = addAt;
-    }
-
-    public LocalDate getDrawDate() {
-        return drawDate;
-    }
-
-    public void setDrawDate(LocalDate drawDate) {
-        this.drawDate = drawDate;
-    }
-
-    public List<Integer> getDrawnNumbers() {
-        return drawnNumbers;
-    }
-
-    public void setDrawnNumbers(List<Integer> drawnNumbers) {
-        this.drawnNumbers = drawnNumbers;
-    }
-
-    public List<Integer> getSortedDrawNumbers() {
-        return sortedDrawNumbers;
-    }
-
-    public void setSortedDrawNumbers(List<Integer> sortedDrawNumbers) {
-        this.sortedDrawNumbers = sortedDrawNumbers;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public LotteryType getLotteryType() {
-        return lotteryType;
-    }
-
-    public void setLotteryType(LotteryType lotteryType) {
-        this.lotteryType = lotteryType;
-    }
 }
