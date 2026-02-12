fb3eb288beb5af6877076a6c723e4d923187ecd9
evertongodoy <everton.mgodoy@gmail.com>
2026-02-11 22:19:21 -0300

    added @RequireRole({UserRole.USER, UserRole.ADMIN}) for createUserDraw

--- src/main/java/com/lottery/validation/infrastructure/adapters/input/rest/controllers/UserController.java
+++ src/main/java/com/lottery/validation/infrastructure/adapters/input/rest/controllers/UserController.java
@@ -82,6 +82,7 @@ public class UserController {
 
     @PostMapping("/create-my-draw")
     @Operation(summary = "Create user draw", description = "Creates a new lottery draw for a user")
+    @RequireRole({UserRole.USER, UserRole.ADMIN})
     public ResponseEntity<UserDrawResponse> createUserDraw(@Valid @RequestBody CreateUserDrawRequest request) {
         log.info("[createUserDraw] Início - Request Body: {}", request);
         var userDrawDTO = userDrawRestMapper.toDTO(request);
